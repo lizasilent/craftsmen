9c6c7496ae17cb34a3f032cff46a2aec7aab8644
lizasilent <elizavetad@yandex.ru>
2022-06-21 23:56:10 +0300

    valid

--- validation.js
+++ validation.js
@@ -1,66 +1,92 @@
 
 
-console.log(document.forms);
+// Функция isValid теперь принимает formElement и inputElement,
+// а не берёт их из внешней области видимости
+
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    // showInputError теперь получает параметром форму, в которой
+    // находится проверяемое поле, и само это поле
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    // hideInputError теперь получает параметром форму, в которой
+    // находится проверяемое поле, и само это поле
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  // Находим элемент ошибки внутри самой функции
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  // Остальной код такой же
+  inputElement.classList.add('contact-us__error-style');
+  // errorElement.textContent = errorMessage;
+  // errorElement.classList.add('form__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  // Находим элемент ошибки
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  // Остальной код такой же
+  inputElement.classList.remove('contact-us__error-style');
+  // errorElement.classList.remove('form__input-error_active');
+  // errorElement.textContent = '';
+};
+
+const setEventListeners = (formElement) => {
+  // Находим все поля внутри формы,
+  // сделаем из них массив методом Array.from
+  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+  const buttonElement = formElement.querySelector('.form__submit');
+
+    // Вызовем toggleButtonState, чтобы не ждать ввода данных в поля
+    toggleButtonState(inputList, buttonElement);
 
 
-const contactForm = document.querySelector(".contact-us__form");
-const contactInput = contactForm.querySelector(".contact-us__input");
-const contactError = contactInput.querySelector(`#${contactInput.id}-error`);
+  // Обойдём все элементы полученной коллекции
+  inputList.forEach((inputElement) => {
+    // каждому полю добавим обработчик события input
+    inputElement.addEventListener('input', () => {
+      // Внутри колбэка вызовем isValid,
+      // передав ей форму и проверяемый элемент
+      isValid(formElement, inputElement)
+    });
+     // Вызовем toggleButtonState и передадим ей массив полей и кнопку
+     toggleButtonState(inputList, buttonElement);
+
+  });
+};
+
+// Функция принимает массив полей
+
+const hasInvalidInput = (inputList) => {
+  // проходим по этому массиву методом some
+  return inputList.some((inputElement) => {
+        // Если поле не валидно, колбэк вернёт true
+    // Обход массива прекратится и вся фунцкция
+    // hasInvalidInput вернёт true
+
+    return !inputElement.validity.valid;
+  })
+};
+
+// Функция принимает массив полей ввода
+// и элемент кнопки, состояние которой нужно менять
+
+const toggleButtonState = (inputList, buttonElement) => {
+  // Если есть хотя бы один невалидный инпут
+  if (hasInvalidInput(inputList)) {
+    // сделай кнопку неактивной
+    buttonElement.classList.add('form__submit_inactive');
+  } else {
+        // иначе сделай кнопку активной
+    buttonElement.classList.remove('form__submit_inactive');
+  }
+};
 
 
 
-console.log(contactInput);
-
-
-
-
-// function setEventListeners (form) {
-//   // Найдём все поля формы и сделаем из них массив
-// const inputList = Array.from(form.querySelectorAll(`.contact-us__input`));
-//   // Найдём в текущей форме кнопку отправки
-// const buttonElement = form.querySelector('.contact-us__btn');
-
-// inputList.forEach((input) => {
-//   input.addEventListener('input', () => {
-//     isValid(input);
-
-//           // Вызовем toggleButtonState и передадим ей массив полей и кнопку
-//     toggleButtonState(inputList, buttonElement);
-//   });
-// });
-// };
-
-
-// function isValid (input) {
-//   if (!input.validity.valid) {
-//     // showInputError получает параметром форму, в которой
-//     // находится проверяемое поле, и само это поле
-//     showInputError(input, input.validationMessage);
-//   } else {
-//     // hideInputError получает параметром форму, в которой
-//     // находится проверяемое поле, и само это поле
-//     hideInputError(input);
-//   }
-// };
-
-
-
-// function showInputError (input, error) {
-//   // Находим элемент ошибки внутри самой функции
-//   const contactError = contactForm.querySelector(`#${contactInput.id}-error`);
-//   // Остальной код такой же
-//   input.classList.add('contact-us__error-style');
-//   error.classList.add('contact-us__error-msg');
-//   contactError.textContent = contactError;
-// };
-
-// function hideInputError (input, error) {
-//   input.classList.remove('contact-us__error-style');
-//   error.classList.remove('contact-us__error-msg');
-//   contactError.textContent = "";
-
-// };
-
+// На случай если будет больше форм
 
 // const enableValidation = () => {
 //   // Найдём все формы с указанным классом в DOM,
@@ -68,46 +94,17 @@ console.log(contactInput);
 //   const formList = Array.from(document.querySelectorAll('.form'));
 
 //   // Переберём полученную коллекцию
-//   formList.forEach((contactForm) => {
-//     contactForm.addEventListener('submit', (evt) => {
+//   formList.forEach((formElement) => {
+//     formElement.addEventListener('submit', (evt) => {
 //       // У каждой формы отменим стандартное поведение
 //       evt.preventDefault();
 //     });
 
 //     // Для каждой формы вызовем функцию setEventListeners,
 //     // передав ей элемент формы
-//     setEventListeners(contactForm);
+//     setEventListeners(formElement);
 //   });
 // };
 
 // // Вызовем функцию
 // enableValidation();
-
-// const hasInvalidInput = (inputList) => {
-//   // проходим по этому массиву методом some
-//   return inputList.some((contactInput) => {
-//         // Если поле не валидно, колбэк вернёт true
-//     // Обход массива прекратится и вся фунцкция
-//     // hasInvalidInput вернёт true
-
-//     return !contactInput.validity.valid;
-//   })
-// };
-
-
-// // Функция принимает массив полей ввода
-// // и элемент кнопки, состояние которой нужно менять
-
-// const toggleButtonState = (inputList, buttonElement) => {
-//   // Если есть хотя бы один невалидный инпут
-//   if (hasInvalidInput(inputList)) {
-//     // сделай кнопку неактивной
-//     buttonElement.classList.add('form__submit_inactive');
-//   } else {
-//         // иначе сделай кнопку активной
-//     buttonElement.classList.remove('form__submit_inactive');
-//   }
-// };
-
-
-
