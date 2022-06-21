c5e620ac96defc484174a4cf7dbe088b8bed25ec
Liza_silent <elizavetad@ya.ru>
2022-06-21 14:51:21 +0300

    validation check

--- validation.js
+++ validation.js
@@ -5,6 +5,7 @@ console.log(document.forms);
 
 const contactForm = document.querySelector(".contact-us__form");
 const contactInput = contactForm.querySelector(".contact-us__input");
+const formError = contactInput.querySelector(`#${formInput.id}__error`);
 
 
 contactForm.addEventListener("submit", function (evt) {
@@ -14,10 +15,31 @@ contactForm.addEventListener("submit", function (evt) {
 });
 
 
-contactInput.addEventListener('input', function (evt) {
-    // Выведем в консоль значение свойства validity.valid поля ввода,
-    // на котором слушаем событие input
-    console.log(evt.target.validity.valid);
+// Функция, которая добавляет класс с ошибкой
+const showInputError = (element, errorMessage) => {
+  element.classList.add('contact-us__input_error');
+  formError.classList.add('contact-us__error-msg_active');
+  formError.textContent = errorMessage;
 
-  });
+};
 
+// Функция, которая убирает класс с ошибкой
+const hideInputError = (element) => {
+  element.classList.remove('contact-us__input_error');
+  formError.classList.add('contact-us__error-msg_active');
+
+};
+
+// Функция, которая проверяет валидность поля
+const isValid = () => {
+  if (!contactInput.validity.valid) {
+    // Если поле не проходит валидацию, покажем ошибку
+    showInputError(contactInput);
+  } else {
+    // Если проходит, скроем
+    hideInputError(contactInput);
+  }
+};
+
+
+contactInput.addEventListener('input', isValid);
