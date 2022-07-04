16b97c637e74c330964d26d9b6b6a0416592c99f
lizasilent <elizavetad@yandex.ru>
2022-07-04 22:57:09 +0300

    marqee started

--- index.js
+++ index.js
@@ -4,3 +4,15 @@
 //   scale: 2,
 //   rotate: '1turn'
 // });
+
+
+// const navCell = document.querySelector(".nav__cell");
+// const cellTextMiddle = navCell.querySelector(".cell__text-middle");
+
+// navCell.addEventListener("hover", function(){
+
+//   console.log("something");
+
+//   // cellTextMiddle.classList.add(".cell__text-middle_active")
+// })
+
