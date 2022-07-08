b923969d5b54445378690e5f6b2ae17dcbcef3d1
Liza_silent <elizavetad@ya.ru>
2022-07-08 15:16:58 +0300

    webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './index.css';
+
 // Header FadeOut
 
 let header = document.querySelector('.header');
@@ -29,3 +31,9 @@ function scrollHandler() {
 window.addEventListener('scroll', scrollHandler);
 
 
+const numbers = [2, 3, 5];
+
+// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // 4, 6, 10
