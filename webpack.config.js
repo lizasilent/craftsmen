363a69f2fb646ef46ac390bd1b98113b41db7983
Liza_silent <elizavetad@ya.ru>
2022-07-08 18:11:07 +0300

    eslint and webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,53 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: {
+    main: './src/index.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'main.js',
+    publicPath: '',
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    port: 8080
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
-        loader: 'babel-loader',
+        use: 'babel-loader',
         exclude: '/node_modules/'
       },
       {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+      {
         test: /\.css$/,
-        loader: [
-          MiniCssExtractPlugin.loader,
-          {
+        use: [MiniCssExtractPlugin.loader, {
             loader: 'css-loader',
             options: {
               importLoaders: 1
             }
           },
           'postcss-loader'
-        ],
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.(png|svg|jpg|gif|woff2)$/,
-        loader: 'file-loader',
+        ]
       },
     ]
   },
-  
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: './src/index.html'
     }),
-    new MiniCssExtractPlugin()
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+
   ]
-};
+} 
